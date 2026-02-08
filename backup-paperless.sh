73cabc2394bec116210c351a5c58399a6d7b93a9
celalettinb-art <celalettinb@gmail.com>
2026-02-08 16:38:02 +0100

    Update backup-paperless.sh

--- backup-paperless.sh
+++ backup-paperless.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
-
 # If not installed, install cifs-utils: apt install -y cifs-utils
 # Create credential file: nano /root/.smbcredentials
 # Add and customize the lines below:
-# username=USERNAME
-# password=PASSWORD
-# domain=WORKGROUP # optional
+#   username=USERNAME
+#   password=PASSWORD
+#   domain=WORKGROUP # optional
 # Modify permissions for the created .smbcredentials file: chmod 600 /root/.smbcredentials
 # Make the script executable: chmod +x /path/to/script.sh
 # Cronjob: 0 3 * * 0 /path/to/script.sh >> /var/log/backup-paperless.log 2>&1
+# Clean up old backup files on Windows, where the SMB share is running, with a Powershell script:
+#   $date = (Get-Date).AddDays(-31)
+#   Get-ChildItem D:\Backup\paperless | Where-Object {$_.LastWriteTime -lt $date} | Remove-Item -Force
 
 #!/bin/bash
 PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
@@ -17,7 +19,7 @@ set -e
 
 # Configuration
 DATE=$(date +%Y-%m-%d)
-SERVER="//192.168.1.8/Backup"
+SERVER="//IP_ADDRESS/Backup"
 EXPORT_DIR="/opt/paperless/export/$DATE"
 REMOTE_DIR="paperless" 
 CREDS="/root/.smbcredentials"
